5b30ac1bbf9c445e68f1db17e56ec30ee1f7ea1b
Anna Local <anna.macinnis109@gmail.com>
2024-03-23 14:27:31 -0400

    Node class + presents hashset

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 // Author: Anna MacInnis, last updated on 3/12/2024
 // Main driver class
 
+import java.util.HashSet;
+
 public class Main 
 {
     public static final int PRESENTS = 500000;
@@ -15,7 +17,14 @@ public class Main
         int [] bagOfPresents = new int [PRESENTS];
         for (int i = 0; i < PRESENTS; i++)
             bagOfPresents[i] = i;
-        // todo: write list class and instantiate here
+
+        // Instantiate present list and present bag.
+        PresentList pList = new PresentList();
+        HashSet<Integer> pBag = new HashSet<>();
+        for (int i = 1; i <= PresentList.PRESENTS; i++)
+        {
+            pBag.add(i);
+        }
 
         // Create thread pool of size 4 (minotaur's 4 servants).
         // todo: make the thread pool
