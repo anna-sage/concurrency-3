0a9dcf9b64345cf377fec6e3c348ee418008cb35
Anna Local <anna.macinnis109@gmail.com>
2024-03-23 14:54:36 -0400

    Func to get present from bag

--- Main.java
+++ Main.java
@@ -2,21 +2,18 @@
 // Main driver class
 
 import java.util.HashSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main 
 {
-    public static final int PRESENTS = 500000;
+    public static final int SERVANTS = 4;
 
     public static void main(String [] args)
     {
         System.out.println("Hello world");
 
         // Problem 1: The Birthday Presents Party
-
-        // Initialize the presents and the linked list to store them.
-        int [] bagOfPresents = new int [PRESENTS];
-        for (int i = 0; i < PRESENTS; i++)
-            bagOfPresents[i] = i;
 
         // Instantiate present list and present bag.
         PresentList pList = new PresentList();
@@ -27,9 +24,14 @@ public class Main
         }
 
         // Create thread pool of size 4 (minotaur's 4 servants).
-        // todo: make the thread pool
+        ExecutorService servants = Executors.newFixedThreadPool(SERVANTS);
         
         // Create tasks to randomly select.
         // todo: task that involves adding then immediately removing
+        // Randomly adds some present from the bag and removes some present from the list.
+        Runnable addAndProcess = () -> {
+            // todo: select a random present to process.
+            int toAdd = pList.getPresentToAdd();
+        };
     }
 }
--- PresentList.java
+++ PresentList.java
@@ -1,9 +1,10 @@
 // List of presents to be accessed concurrently by the minotaur's servants.
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PresentList
 {
-    // Fields:
-
     // Tracker nodes.
     private PNode head; // Points to sentinel.
     private PNode tail;
@@ -11,12 +12,43 @@ public class PresentList
     // Amount of presents.
     public static final int PRESENTS = 500000;
 
+    // Bag of presents.
+    private ArrayList<Integer> presentBag;
+    private ArrayList<Integer> toRemove;
+
+    // todo delete
+    private final boolean DEBUGGING = true;
+
     public PresentList() 
     {
         // Generate first sentinel node (does not represent a present in the list).
         head = new PNode(PRESENTS + 1, null);
         tail = head;
+
+        // Fill the present bag with all the presents.
+        presentBag = new ArrayList<>();
+        for (int i = 0; i < PRESENTS; i++)
+        {
+            presentBag.add(i);
+        }
+
+        toRemove = new ArrayList<>();
     }
+
+    // Get a present id from the bag.
+    public synchronized int getPresentToAdd()
+    {
+        Random rand = new Random();
+        int idx = rand.nextInt(presentBag.size());
+        int retVal = presentBag.remove(idx);
+        if (DEBUGGING) System.out.println("idx: " + idx + ", retVal: " + retVal);
+        return retVal;
+    }
+
+    // public synchronized int getPresentToRemove()
+    // {
+
+    // }
 
     // Node representing a single present.
     private class PNode
